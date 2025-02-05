73259f3dac66a219a5773cb52cbdf388c323f1f3
Ash (Bagel) <ash@rjohnston.net>
2025-02-05 13:25:28 -0600

    getting my point across better

--- src/main/java/dev/bagel/extra_apoth_compat/ModMixinConfig.java
+++ src/main/java/dev/bagel/extra_apoth_compat/ModMixinConfig.java
@@ -10,7 +10,7 @@ public class ModMixinConfig {
     public static void load() {
         Configuration cfg = new Configuration(ApothicAttributes.getConfigFile(ExtraApothCompat.MODID + "_mixins"));
         disabledMixins = cfg.getStringList("Disabled Mixins", "disable", new String[]{""}, "Disabled Mixins. Only use if you know what you're doing! Check the class structure here (https://github.com/TheWinABagel/Apoth-Compat/tree/main/src/main/java/dev/bagel/extra_apoth_compat/mixin) first.");
-        disableJeiLoading = cfg.getBoolean("Disable JEI Loading", "disable", true, "If Apotheosis JEI support loading should be cancelled via mixin. May *very* slightly improve load times if EMI and JEI are loaded simultaneously.");
+        disableJeiLoading = cfg.getBoolean("Disable JEI Loading", "disable", true, "If Apotheosis JEI support loading should be cancelled via mixin. If disabled, and JEI and EMI are both loaded, there will be duplicate recipes in EMI!");
         if (cfg.hasChanged()) {
             cfg.save();
         }
